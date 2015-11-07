b38c41ea91962ffb83709956bf11969113be66a5
枫弦 <fengxian.yzg@alibaba-inc.com>
2015-11-07 18:47:09 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+/*!
+ * 1314: index.js
+ * Authors  : 枫弦 <fengxian.yzg@alibaba-inc.com> (https://github.com/yuzhigang33)
+ * Create   : 2015-11-07 18:46:52
+ * CopyRight 2015 (c) Alibaba Group
+ */
+console.log('1314');
